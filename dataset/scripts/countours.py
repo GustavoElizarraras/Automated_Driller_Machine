d71739622d1041058866d4208936ce29ade95fec
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2022-05-01 21:05:22 -0500

    added more Hough circles parameters for imgs

--- dataset/scripts/countours.py
+++ dataset/scripts/countours.py
@@ -1,19 +1,26 @@
 import cv2
-pcb1 = cv2.imread('dataset/hand_picked/12100146_original.jpg')
-pcb1_bin = cv2.bitwise_not(cv2.imread('dataset/hand_picked/12100146_original.jpg', 0))
+pcb1 = cv2.imread('dataset/hand_picked/13000186_original.jpg')
+pcb1_bin = cv2.bitwise_not(cv2.imread('dataset/hand_picked/13000186_original.jpg', 0))
 grosor=1
 fuente=cv2.FONT_HERSHEY_COMPLEX
 
+# cv2.imshow("img1", pcb1)
+# cv2.waitKey(0)
 # 120 images: param1 = 50, param2 = 8, minRadius = 1, maxRadius = 6
 # 121 images: param1 = 50, param2 = 13, minRadius = 7, maxRadius = 23
+# 123 images: param2 = 12, minRadius = 13, maxRadius = 24
+# 130 images: param2 = 13, minRadius = 8, maxRadius = 24
 detected_circles = cv2.HoughCircles(pcb1_bin, 
                    cv2.HOUGH_GRADIENT, 1, 20, param1 = 50,
-               param2 = 13, minRadius = 7, maxRadius = 23)
+               param2 = 13, minRadius = 8, maxRadius = 24)
 
 for pt in detected_circles[0, :]:
     # circle coords
     a, b, r = int(pt[0]), int(pt[1]), int(pt[2])
-
+    r2 = int(0.65*r)
+    cv2.circle(pcb1, (a, b), r2, (255, 255, 255), -1)
+    # cv2.imshow("img1", pcb1)
+    # cv2.waitKey(0)
     # Draw the circumference of the circle.
     cv2.circle(pcb1, (a, b), r, (0, 255, 0), 2)
 
