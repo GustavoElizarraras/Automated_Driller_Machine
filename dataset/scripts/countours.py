f6ac3156e395f1aad0f5d0d1e48fd87d66b2641e
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2022-07-25 16:07:10 -0500

    dilation added to explore images

--- dataset/scripts/countours.py
+++ dataset/scripts/countours.py
@@ -1,33 +1,44 @@
 import cv2
-pcb1 = cv2.imread('dataset/hand_picked/92000020_original.jpg')
-pcb1_bin = cv2.bitwise_not(cv2.imread('dataset/hand_picked/92000020_original.jpg', 0))
+import numpy as np
+pcb1 = cv2.imread('dataset/hand_picked_originals/13000027_test.jpg')
+pcb1_bin = cv2.bitwise_not(cv2.imread('dataset/hand_picked_originals/13000027_test.jpg', 0))
+
 grosor=1
 fuente=cv2.FONT_HERSHEY_COMPLEX
 
-# cv2.imshow("img1", pcb1)
-# cv2.waitKey(0)
-# 120 images: param1 = 50, param2 = 8, minRadius = 1, maxRadius = 6
-# 121 images: param1 = 50, param2 = 13, minRadius = 7, maxRadius = 23
-# 123 images: param2 = 12, minRadius = 13, maxRadius = 24
-# 130 images: param2 = 13, minRadius = 8, maxRadius = 24
+# ret,pcb1 = cv2.threshold(pcb1, 127, 255,cv2.THRESH_BINARY)
+
+cv2.imshow("img00", pcb1_bin)
+cv2.waitKey(0)
+# morph_erosion = cv2.erode(pcb1, (15,15),iterations = 1)
+kernel = np.ones((3,3),np.uint8)
+pcb1_bin = cv2.erode(pcb1_bin, kernel,iterations = 1)
+
+cv2.imshow("img0", pcb1_bin)
+cv2.waitKey(0)
+cv2.imshow("img0", cv2.bitwise_not(pcb1_bin))
+cv2.waitKey(0)
+# 120 images: param1 = 50, param2 = 8, minRadius = 1, maxRadius = 6, kernel = (7,7)
+# 121 images: param1 = 50, param2 = 15, minRadius = 8, maxRadius = 23, kernel= (3,3)
+# 123 images: param2 = 14, minRadius = 12, maxRadius = 23. kernel = (7,7)
+# 130 images: param2 = 13, minRadius = 8, maxRadius = 24, kernel = (3,3)
 # 901 images: param2 = 13, minRadius = 8, maxRadius = 20
 # 920 images: param2 = 11, minRadius = 5, maxRadius = 10
 detected_circles = cv2.HoughCircles(pcb1_bin, 
                    cv2.HOUGH_GRADIENT, 1, 20, param1 = 50,
-               param2 = 11, minRadius = 5, maxRadius = 10)
+               param2 = 14, minRadius = 8, maxRadius = 19)
 
 for pt in detected_circles[0, :]:
     # circle coords
     a, b, r = int(pt[0]), int(pt[1]), int(pt[2])
-    r2 = int(0.62*r)
+    r2 = int(1.1*r)
     cv2.circle(pcb1, (a, b), r2, (255, 255, 255), -1)
-    # cv2.imshow("img1", pcb1)
-    # cv2.waitKey(0)
+
     # Draw the circumference of the circle.
     cv2.circle(pcb1, (a, b), r, (0, 255, 0), 2)
 
     # Draw a small circle (of radius 1) to show the center.
-    cv2.circle(pcb1, (a, b), 1, (0, 0, 255), 3)
+    # cv2.circle(pcb1, (a, b), 1, (0, 0, 255), 3)
 
-cv2.imshow("img", pcb1)
+cv2.imshow("circles", pcb1)
 cv2.waitKey(0)
