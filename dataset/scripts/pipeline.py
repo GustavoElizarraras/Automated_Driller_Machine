b000dd19c19947d90d33cf945daabcaf37ebf2e3
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2022-07-27 00:22:20 -0500

    sorted files and own csv

--- dataset/scripts/pipeline.py
+++ dataset/scripts/pipeline.py
@@ -6,23 +6,23 @@ import csv
 
 images_path = os.getcwd() + "/dataset/hand_picked_originals"
 parameters = {
-    "120": { "param2" :  9, "minRadius":   7, "maxRadius":  9, "kernel": (1,1) },
+    "120": { "param2" :  8, "minRadius":   6, "maxRadius":  9, "kernel": (1,1) },
     "121": { "param2" : 15, "minRadius":   8, "maxRadius": 23, "kernel": (3,3) },
     "123": { "param2" : 14, "minRadius":  12, "maxRadius": 23, "kernel": (7,7) },
     "130": { "param2" : 13, "minRadius":   8, "maxRadius": 24, "kernel": (3,3) },
-    "901": { "param2" : 13, "minRadius":   9, "maxRadius": 20, "kernel": (2,2) },
-    "920": { "param2" : 11, "minRadius":   5, "maxRadius": 10, "kernel": (7,7) }
+    "901": { "param2" : 11, "minRadius":   9, "maxRadius": 20, "kernel": (2,2) },
+    "920": { "param2" :  8, "minRadius":   5, "maxRadius": 10, "kernel": (7,7) }
 }
 rotations = [None, cv2.ROTATE_90_CLOCKWISE, cv2.ROTATE_90_COUNTERCLOCKWISE, cv2.ROTATE_180]
 n1 = random.randint(0,3)
 for root, dir, files in os.walk(images_path):
-    for image in files:
+    for image in sorted(files):
         n2 = random.randint(0,3)
         while n1 == n2:
             n2 = random.randint(0,3)
         # reading image
         image_type = image[:3]
-        complete_path = "dataset/edited_images" + "/" + image_type + "/" + image
+        complete_path = "dataset/edited_images" + "/" + image_type + image
 
         pcb = cv2.imread(root + "/" + image)
         pcb_bin = cv2.bitwise_not(cv2.imread(root + "/" + image, 0))
@@ -33,7 +33,7 @@ for root, dir, files in os.walk(images_path):
         if image_type == "901":
             pcb_erode = cv2.dilate(pcb_bin, parameters[image_type]["kernel"],iterations = 1)
 
-        pcb_rotated = cv2.rotate(pcb_erode, rotations[n])
+        pcb_rotated = cv2.rotate(pcb_erode, rotations[n2])
 
         detected_circles = cv2.HoughCircles(pcb_rotated, cv2.HOUGH_GRADIENT, 1, 20, param1 = 50,
                     param2 = parameters[image_type]["param2"],
@@ -65,7 +65,7 @@ for root, dir, files in os.walk(images_path):
                 locations.append(y1)
                 locations.append(x2)
                 locations.append(y2)
-            n1 = random.randint(0,3)
+            n1 = n2
             with open("dataset/scrapped_locations.csv", "a") as w:
                 writer = csv.writer(w)
                 writer.writerow(locations)
