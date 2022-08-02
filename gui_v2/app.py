d85eb0ab7061b70bb06e267f005ee6bd6a8f99e9
Gustavo Elizarraras <angelizarrarasg@gmail.com>
2022-08-02 01:30:48 -0500

    initialize the image in every frame w/class

--- gui_v2/app.py
+++ gui_v2/app.py
@@ -3,33 +3,35 @@ from tkinter import ttk
 from tkinter.messagebox import showerror
 from PIL import Image, ImageTk
 
-class ControlFrame(ttk.LabelFrame):
+class ImageInitializer(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
         self.container = container
-        # self.container = container
         # PCB Image
         self.img = Image.open("gui/12000001_temp.jpg")
         self.render = ImageTk.PhotoImage(self.img)
-        self.img_label = ttk.Label(self.master, image=self.render, compound="right")
+        self.img_label = ttk.Label(self.container, image=self.render, compound="right")
         self.img_label.place(x = 10, y = 10)
 
+class ControlFrame(ImageInitializer):
+    def __init__(self, container):
+        super().__init__(container)
         self.create_widgets()
 
     def create_widgets(self):
 
         # button
-        self.add_button = ttk.Button(self.master, text='Añadir barreno')
+        self.add_button = ttk.Button(self.container, text='Añadir barreno')
         self.add_button.place(x = 700, y = 10)
         self.add_button.configure(command=self.press_add)
 
         # button
-        self.del_button = ttk.Button(self.master, text='Eliminar barreno')
+        self.del_button = ttk.Button(self.container, text='Eliminar barreno')
         self.del_button.place(x = 700, y = 50)
         # self.convert_button.configure(command=self.convert)
 
         # button
-        self.move_button = ttk.Button(self.master, text='Mover barreno')
+        self.move_button = ttk.Button(self.container, text='Mover barreno')
         self.move_button.place(x = 700, y = 90)
         # self.convert_button.configure(command=self.convert)
 
@@ -48,7 +50,7 @@ class ControlFrame(ttk.LabelFrame):
 #     def press_move(self):
 #         pass
 
-class AddPCBHole(ttk.Frame):
+class AddPCBHole(ImageInitializer):
     def __init__(self, container):
         super().__init__(container)
         self.create_widgets()
