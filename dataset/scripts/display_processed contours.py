bc15b8d133255a3eb95fbbbd76f65726fce085f2
Gustavo <angelizarrarasg@gmail.com>
2022-10-05 14:01:14 -0500

    scrapped data correctly labeled and processed

---
+++ dataset/scripts/display_processed contours.py
@@ -0,0 +1,51 @@
+import cv2
+import os
+import numpy as np
+import csv
+import time
+
+# pcb1 = cv2.imread('dataset/hand_picked/92000020_original.jpg')
+# pcb1_bin = cv2.bitwise_not(cv2.imread('dataset/hand_picked/92000020_original.jpg', 0))
+grosor=1
+fuente=cv2.FONT_HERSHEY_COMPLEX
+
+gimp_path = os.getcwd() + "/dataset/pcb_gimp_morph"
+
+with open("dataset/scrapped_locations.csv", "r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        img =  os.getcwd() + "/dataset/pcb_gimp_morph/" + row[0]
+        pcb = cv2.imread(img)
+        #centers = []
+        for i in range(1, len(row), 4):
+            x1, y1 = int(row[i]), int(row[i+1])
+            x2, y2 = int(row[i+2]), int(row[i+3])
+            half_x = (x2-x1) // 2
+            half_y = (y1-y2) // 2
+            #centers.append(((x2-x1)/2, (y2-y1)/2))
+
+            cv2.circle(pcb, (x1 + half_x, y1 - half_y), int((y1-y2)/2), (0, 255, 0), 2)
+
+        cv2.imshow("img", pcb)
+        cv2.waitKey(0)
+        #print(row)
+
+# for root, dir, files in os.walk(gimp_path):
+#     for image in files:
+#         cb1 = cv2.imread(gimp_path + "/" + image)p
+#         pcb1_bin = cv2.bitwise_not(cv2.imread(gimp_path + "/" + image, 0))
+
+#         detected_circles = cv2.HoughCircles(pcb1_bin, cv2.HOUGH_GRADIENT, 1, 20, param1= 50,
+#                                             param2 = 9, minRadius = 5, maxRadius = 11)
+
+#         for pt in detected_circles[0, :]:
+#             # circle coords
+#             a, b, r = int(pt[0]), int(pt[1]), int(pt[2])
+#             # Draw the circumference of the circle.
+#             cv2.circle(pcb1, (a, b), r, (0, 255, 0), 2)
+
+#             # Draw a small circle (of radius 1) to show the center.
+#             cv2.circle(pcb1, (a, b), 1, (0, 0, 255), 3)
+
+#         cv2.imshow("img", pcb1)
+#         cv2.waitKey(0)
--- dataset/scripts/pipeline_scrapped.py
+++ dataset/scripts/pipeline_scrapped.py
@@ -12,22 +12,18 @@ for root, dir, files in os.walk(gimp_path):
     for image in files:
         pcb = cv2.imread(gimp_path + "/" + image)
         pcb_bin = cv2.bitwise_not(cv2.imread(gimp_path + "/" + image, 0))
-        # random erosion
-        n = random. randint(0,1)
-        if n == 1:
-            kernel = np.ones((3, 3),np.uint8)
-            pcb_erode = cv2.erode(pcb_bin, kernel,iterations = 1)
-        else:
-            pcb_erode = pcb_bin
+        # a4988
         # rotation
         for i, rotation in enumerate(rotations):
-            pcb_erode = cv2.rotate(pcb_erode, rotation)
+            if i == 0:
+                pcb_rotated = pcb_bin
+            else:
+                pcb_rotated = cv2.rotate(pcb_bin, rotation)
             image_name = image[:-4] + f"_{i}" + ".jpg"
-            cv2.imwrite(new_gimp_path + image_name, cv2.bitwise_not(pcb_erode))
-            # detected circles
-            detected_circles = cv2.HoughCircles(pcb_bin, cv2.HOUGH_GRADIENT, 1, 20, param1 = 50,
-                                                param2 = 9, minRadius = 5, maxRadius = 11)
             locations = [image_name]
+            # detected circles
+            detected_circles = cv2.HoughCircles(pcb_rotated, cv2.HOUGH_GRADIENT, 1, 20, param1 = 50,
+                                                param2 = 9, minRadius = 5, maxRadius = 11)
             for pt in detected_circles[0, :]:
                 # circle coords
                 a, b, r = int(pt[0]), int(pt[1]), int(pt[2])
@@ -38,6 +34,17 @@ for root, dir, files in os.walk(gimp_path):
                 locations.append(y1)
                 locations.append(x2)
                 locations.append(y2)
+
+            # writing the locations of the original image
             with open("dataset/scrapped_locations.csv", "a") as w:
                 writer = csv.writer(w)
                 writer.writerow(locations)
+
+            # random erosion
+            n = random. randint(0,1)
+            pcb_rotated_eroded = pcb_rotated
+            if n == 1:
+                kernel = np.ones((3, 3),np.uint8)
+                pcb_rotated_eroded = cv2.erode(pcb_rotated, kernel,iterations = 1)
+
+            cv2.imwrite(new_gimp_path + image_name, cv2.bitwise_not(pcb_rotated_eroded))
