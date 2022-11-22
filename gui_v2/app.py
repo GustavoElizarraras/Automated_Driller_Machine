f89581327f933c8cf3cc728df5216daaee473191
Gustavo <angelizarrarasg@gmail.com>
2022-11-22 17:24:52 -0600

    black and white img for preprocessing

--- gui_v2/app.py
+++ gui_v2/app.py
@@ -78,8 +78,8 @@ class BinarizingFrame(ttk.Frame):
         # self.img_array = cv2.merge([np.asarray(self.img), np.asarray(self.img), np.asarray(self.img)])
 
         self.img_array = np.asarray(self.img)
-
-        self.render_img(self.img_array)
+        self.ch = cv2.imread(self.img_path, 0)
+        self.render_img(self.ch)
         self.create_widgets()
 
     def render_img(self, img_array):
@@ -91,19 +91,19 @@ class BinarizingFrame(ttk.Frame):
 
         self.slider1 = tk.Scale(self.container, from_=0, to=255, orient="horizontal")
         self.slider1.bind("<ButtonRelease-1>", self.binarize)
-        self.slider1.place(x=700, y=10)
+        self.slider1.place(x=700, y=50)
 
         self.slider1_2 = tk.Scale(self.container, from_=0, to=255, orient="horizontal")
         self.slider1_2.bind("<ButtonRelease-1>", self.binarize)
-        self.slider1_2.place(x=700, y=50)
+        self.slider1_2.place(x=700, y=100)
 
         self.slider2 = tk.Scale(self.container, from_=0, to=5, orient="horizontal")
         self.slider2.bind("<ButtonRelease-1>", self.dilate)
-        self.slider2.place(x=700, y=90)
+        self.slider2.place(x=700, y=150)
 
         self.slider3 = tk.Scale(self.container, from_=0, to=5, orient="horizontal")
         self.slider3.bind("<ButtonRelease-1>", self.erode)
-        self.slider3.place(x=700, y=130)
+        self.slider3.place(x=700, y=200)
 
         # button
         self.continue_button = ttk.Button(self.container, text='Detectar barrenos')
@@ -113,19 +113,19 @@ class BinarizingFrame(ttk.Frame):
     def binarize(self, event):
         threshold_low = self.slider1.get()
         threshold_high = self.slider1_2.get()
-        _, new_image = cv2.threshold(self.img_array, threshold_low , threshold_high, cv2.THRESH_BINARY)
+        _, new_image = cv2.threshold(self.ch, threshold_low , threshold_high, cv2.THRESH_BINARY)
         self.render_img(new_image)
 
     def dilate(self, event):
         k = self.slider2.get()
         kernel = np.ones((k,k), np.uint8)
-        new_image = cv2.dilate(self.img_array, kernel, iterations = 1)
+        new_image = cv2.dilate(self.ch, kernel, iterations = 1)
         self.render_img(new_image)
 
     def erode(self, event):
         k = self.slider3.get()
         kernel = np.ones((k,k), np.uint8)
-        new_image = cv2.erode(self.img_array, kernel, iterations = 1)
+        new_image = cv2.erode(self.ch, kernel, iterations = 1)
         self.render_img(new_image)
 
     def display_control_panel(self):
