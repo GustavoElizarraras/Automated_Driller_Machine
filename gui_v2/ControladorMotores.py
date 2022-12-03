dc9ba507f4a20e5e07f8cb5a14a2a3fcdc9916bc
Gustavo <angelizarrarasg@gmail.com>
2022-12-02 19:03:32 -0600

    changed name of functions and variables

--- gui_v2/ControladorMotores.py
+++ gui_v2/ControladorMotores.py
@@ -3,49 +3,50 @@ import RPi.GPIO as GPIO
 # Use BCM GPIO references
 # Instead of physical pin numbers
 GPIO.setmode(GPIO.BCM)
-# Define GPIO signals to use Pins 
+# Define GPIO signals to use Pins
 
-PinsOutput = [2,3,4,14,15,18,17,27,22,23,24]
-PinsInput=[25,8,7,1]
+output_pins = [2,3,4,14,15,18,17,27,22,23,24]
+input_pins = [25,8,7,1]
 # Set all pins as output and input
 GPIO.setwarnings(False)
 
-for pin in PinsOutput:   
+for pin in output_pins:
     GPIO.setup(pin,GPIO.OUT)
     GPIO.output(pin,False)
 
-for pin in PinsInput:   
+for pin in input_pins:
     GPIO.setup(pin,GPIO.IN)
-ctrx=0
-ctry=0
-ctrz=0
-ctrm=0
-ctrp=0
-MotorX=[2,3]
-MotorY=[4,14]
-MotorZ=[15,18]
-MotorM=[17,27]
-MotorP=[22,23]
-direcx=True
-direcy=True
-direcz=True
-direcm=True
-direcp=True
 
-def pulso(PWM):
+ctrx = 0
+ctry = 0
+ctrz = 0
+ctrm = 0
+ctrp = 0
+motor_x = [2,3]
+motor_y = [4,14]
+motor_z = [15,18]
+motor_mesa = [17,27]
+motor_pistones = [22,23]
+direc_x = True
+direc_y = True
+direc_z = True
+direc_mesa = True
+direc_pistones = True
+
+def mandar_pulso(pwm_pin):
     # 1KHz
-    GPIO.output(PWM, False)
+    GPIO.output(pwm_pin, False)
     time.sleep(0.00025)
-    GPIO.output(PWM, True)
+    GPIO.output(pwm_pin, True)
     time.sleep(0.0005)
-    GPIO.output(PWM, False)
+    GPIO.output(pwm_pin, False)
     time.sleep(0.00025)
-    
-def Motor_bloque(pulsos,direcc,motor):   
+
+def mover_motor(pulsos,direcc,motor):
     ctr=0
     GPIO.output(motor[0], direcc)
     for i in range(pulsos):
-        pulso(motor[1])
+        mandar_pulso(motor[1])
         if direcc:
             ctr=ctr+1
         else:
@@ -58,34 +59,34 @@ def Motor_bloque(pulsos,direcc,motor):
             GPIO.output(24,False)
     return ctr
 
-def Inicio():
-    
-    Motor_bloque(10000,not direcz,MotorZ)
-    Motor_bloque(200,direcz,MotorZ)
+def inicio():
 
-    Motor_bloque(10000,not direcx,MotorX)
-    Motor_bloque(200,direcx,MotorX)
+    mover_motor(10000,not direc_z,motor_z)
+    mover_motor(200,direc_z,motor_z)
 
-    Motor_bloque(10000,not direcy,MotorY)
-    Motor_bloque(9000,direcy,MotorY)
-    
-    crtpm += crtpm+Motor_bloque(1000,direcm,MotorM)
+    mover_motor(10000,not direc_x,motor_x)
+    mover_motor(200,direc_x,motor_x)
 
-def Sujecion(ancho):
-    ctrp += Motor_bloque(ancho,direcp,MotorP)
+    mover_motor(10000,not direc_y,motor_y)
+    mover_motor(9000,direc_y,motor_y)
 
-def Home():
-    Motor_bloque(10000,not direcz,MotorZ)
-    ctrz += Motor_bloque(200,direcz,MotorZ)
+    crtpm += crtpm+mover_motor(1000,direc_mesa,motor_mesa)
 
-    Motor_bloque(14000,not direcx,MotorX)
-    ctrx += Motor_bloque(5350,direcx,MotorX)
+def sujecion(ancho):
+    ctrp += mover_motor(ancho,direc_pistones,motor_pistones)
 
-    Motor_bloque(14000,not direcy,MotorY)
-    ctry += Motor_bloque(10500,direcy,MotorY)
+def home():
+    mover_motor(10000,not direc_z,motor_z)
+    ctrz += mover_motor(200,direc_z,motor_z)
 
-def MesaSetup():
-    ctrm=ctrm+Motor_bloque(ctrm,not direcm,MotorM)
+    mover_motor(14000,not direc_x,motor_x)
+    ctrx += mover_motor(5350,direc_x,motor_x)
+
+    mover_motor(14000,not direc_y,motor_y)
+    ctry += mover_motor(10500,direc_y,motor_y)
+
+def setup_mesa():
+    ctrm=ctrm+mover_motor(ctrm,not direc_mesa,motor_mesa)
 
 
 
